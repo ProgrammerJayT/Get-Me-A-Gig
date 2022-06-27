5ecdfd9b65071ca52f479b413e273710eeea527e
ProgrammerJayT <theanthem8@gmail.com>
2022-06-27 16:49:17 +0200

    Resized logo image size

--- Mobile/Get_Me_A_Gig/screens/Splash.js
+++ Mobile/Get_Me_A_Gig/screens/Splash.js
@@ -1,7 +1,7 @@
-import {StyleSheet, Text, SafeAreaView, Image} from 'react-native';
+import {StyleSheet, Text, SafeAreaView, Image, Dimensions} from 'react-native';
 import React from 'react';
 
-const Splash = ({imageURL}) => {
+const Splash = () => {
   return (
     <SafeAreaView style = {styles.body}>
       <Image source = {require('../res/images/logo.png')} style = {styles.image} />
@@ -14,17 +14,12 @@ export default Splash;
 const styles = StyleSheet.create({
     body: {
         flex: 1,
-        backgroundColor: '#000',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
     image: {
-        width: 250,
-        height: 250,
-        resizeMode: 'contain',
-        overflow: 'hidden',
-        borderRadius: 1000,
-        borderColor: '#fff',
-        borderWidth: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width,
     }
 });
