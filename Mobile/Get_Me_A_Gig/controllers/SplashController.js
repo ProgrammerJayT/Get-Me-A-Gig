afdd313e8e46380ab6f069a1c283f50d7e25ba58
ProgrammerJayT <theanthem8@gmail.com>
2022-06-27 11:56:13 +0200

    Created splash controller

---
+++ Mobile/Get_Me_A_Gig/controllers/SplashController.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Splash from '../screens/Splash';
+
+export default function SplashController() {
+  return (
+    <Splash />
+  );
+}
