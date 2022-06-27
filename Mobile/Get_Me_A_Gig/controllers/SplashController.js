10172b58829bfbb441cf42b73a383281d47cdd8a
ProgrammerJayT <theanthem8@gmail.com>
2022-06-27 12:19:46 +0200

    Added logo

--- Mobile/Get_Me_A_Gig/controllers/SplashController.js
+++ Mobile/Get_Me_A_Gig/controllers/SplashController.js
@@ -2,8 +2,10 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import Splash from '../screens/Splash';
 
-export default function SplashController() {
+export default function SplashController({navigation}) {
+
+  const imageURL = '../res/images/logo.png';
   return (
-    <Splash />
+    <Splash imageURL = {imageURL} />
   );
 }
--- Mobile/Get_Me_A_Gig/screens/Splash.js
+++ Mobile/Get_Me_A_Gig/screens/Splash.js
@@ -1,10 +1,10 @@
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, Text, SafeAreaView, Image} from 'react-native';
 import React from 'react';
 
-const Splash = () => {
+const Splash = ({imageURL}) => {
   return (
     <SafeAreaView style = {styles.body}>
-      <Text>Splash</Text>
+      <Image source = {require('../res/images/logo.png')} style = {styles.image} />
     </SafeAreaView>
   );
 };
@@ -14,8 +14,17 @@ export default Splash;
 const styles = StyleSheet.create({
     body: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    image: {
+        width: 250,
+        height: 250,
+        resizeMode: 'contain',
+        overflow: 'hidden',
+        borderRadius: 1000,
+        borderColor: '#fff',
+        borderWidth: 1,
     }
 });
